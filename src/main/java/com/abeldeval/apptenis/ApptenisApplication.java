e151cfff4f8cc62f96348e6a6b31debca5f925f3
Abel <abeld89@hotmail.com>
2021-09-01 19:03:02 +0200

    Init project

---
+++ src/main/java/com/abeldeval/apptenis/ApptenisApplication.java
@@ -0,0 +1,13 @@
+package com.abeldeval.apptenis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApptenisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApptenisApplication.class, args);
+	}
+
+}
