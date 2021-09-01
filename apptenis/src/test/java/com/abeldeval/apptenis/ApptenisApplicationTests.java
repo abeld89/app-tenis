dd7b46b78e18d9253322cd87dbe417032096d068
Abel <abeld89@hotmail.com>
2021-09-01 19:01:37 +0200

    Init project

---
+++ apptenis/src/test/java/com/abeldeval/apptenis/ApptenisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abeldeval.apptenis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApptenisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
